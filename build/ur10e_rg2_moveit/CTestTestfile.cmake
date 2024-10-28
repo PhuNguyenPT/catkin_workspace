62a8e3d6e3f696131a458e4f207e39efe9cb50fd
tue <lamthanhtue011103@gmail.com>
2024-10-28 22:14:06 +0700

    delete build and devel

--- build/ur10e_rg2_moveit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/phu/catkin_ws/src/ur10e_rg2_moveit
-# Build directory: /home/phu/catkin_ws/build/ur10e_rg2_moveit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
